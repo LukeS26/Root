58f496da92cd6f5aba281078eef76ffc57bf2975
LukeS26 <LukeS062022@gmail.com>
2023-02-03 21:48:44 -0500

    Added controls

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public int availableMoves;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
