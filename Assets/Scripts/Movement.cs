cd90b3159276640b8d126a1257e1877fc7bfb5ad
LukeS26 <LukeS062022@gmail.com>
2023-02-04 00:21:07 -0500

    Movement working

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,10 +7,14 @@ public class Movement : MonoBehaviour
     public int availableMoves;
     private InputManager inputManager;
 
+    private Rigidbody2D rigidbody;
+
     private bool canMove = false;
 
     void Awake() {
         inputManager = new InputManager();
+
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
     }
 
 
@@ -26,22 +30,26 @@ public class Movement : MonoBehaviour
         float x = inputManager.Plant.Movement.ReadValue<Vector2>().x;
         float y = inputManager.Plant.Movement.ReadValue<Vector2>().y;
 
-        if(Mathf.Abs(x) < 0.1f && Mathf.Abs(y) < 0.1f) {
+        if(Mathf.Abs(x) < 0.5f && Mathf.Abs(y) < 0.5f) {
             canMove = true;
         } else if (canMove && Mathf.Abs(x - y) > 0.2f) {
             canMove = false;
             
             if(Mathf.Abs(y) > Mathf.Abs(x)) {
                 if(y > 0) {
-                    Debug.Log("UP");
+                    Debug.Log(y);
+                    rigidbody.MovePosition( rigidbody.position + new Vector2(0, 1) );
                 } else {
-                    Debug.Log("DOWN");
+                    Debug.Log(y);
+                    rigidbody.MovePosition( rigidbody.position + new Vector2(0, -1) );
                 }
             } else {
                 if(x > 0) {
-                    Debug.Log("RIGHT");
+                    Debug.Log(x);
+                    rigidbody.MovePosition( rigidbody.position + new Vector2(1, 0) );
                 } else {
-                    Debug.Log("LEFT");
+                    Debug.Log(x);
+                    rigidbody.MovePosition( rigidbody.position + new Vector2(-1, 0) );
                 }
             }
         }
